0817e50ed0a54a753a522126fb3cb0f90b736127
nuzhatanika560-droid <nuzhatanika560@gmail.com>
2026-09-01 17:38:36 +0600

    python 1.py

--- python.py
+++ python.py
@@ -2,33 +2,32 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+WORKING_RESPONSE = {
+    "maintenance_msg": "Server is under maintenance. Please try later.",
+    "abhotupdate_cdn_url": "https://ffp.springmusk.dev/hotpatchs/",
+    "is_server_open": True,
+    "patchnote_url": "https://t.me/nepgamer",
+    "billboard_cdn_url": "https://ff.springmusk.dev/ffmax/pausedg.jpg;https://ff.springmusk.dev/ffmax/564c3e03-2204-476d-89c7-365b8fbd22f4.png",
+    "billboard_bg_url": "",
+    "billboard_msg": "",
+    "web_url": "https://t.me/nepgamer",
+    "test_url": "https://t.me/nepgamer",
+    "is_update_btn_show": False,
+    "show_high_framerate_UI": True,
+    "appstore_url": "https://t.me/nepgamer",
+    "backup_appstore_url": "https://t.me/nepgamer"
+}
+
+@app.route('/ver.php', methods=['GET', 'POST'])
+def ver():
+    print("PATH:", request.full_path)
+    return jsonify(WORKING_RESPONSE)
+
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
-    print("FULL PATH:", request.full_path)
-    print("METHOD:", request.method)
-    print("HEADERS:", dict(request.headers))
-    print("BODY:", request.get_data(as_text=True))
-
-    return jsonify({
-        "status": 0,
-        "message": "ANNIE_CONFIG_ACTIVE",
-        "login": "ok",
-        "config": {
-            "aim_assist": 100,
-            "aim_assist_range": 999,
-            "aim_assist_angle": 180,
-            "headshot_rate": 100,
-            "aim_target": "head",
-            "no_recoil": 1,
-            "no_spread": 1,
-            "auto_fire": 1,
-            "anti_ban": 1,
-            "check_version": 0,
-            "check_resource": 0,
-            "check_hacker": 0
-        }
-    })
+    print("PATH:", request.full_path)
+    return jsonify(WORKING_RESPONSE)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
